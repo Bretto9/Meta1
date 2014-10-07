dbd8971fae8e0e2a79a42921359607cc50df8a61
Bretto9 <jose.bretto@gmail.com>
2014-10-07 19:15:41 +0200

    fin del almacenamiento de flujo/distancia

--- Practica 1/main.cpp
+++ Practica 1/main.cpp
@@ -34,9 +34,58 @@ int main(int argc, char** argv) {
             for(int i = 0; i < nCasos; i++){
                 flujos[i] = new int[nCasos];
             }
-    
-        int **distancias;
-
+            getline(flujo, temp);
+            int tempi = 0 ;
+            int tempj = 0 ;
+//            getline(flujo, temp);
+//            cout << temp.substr(0, temp.find(' ')) << " ";
+//            temp = temp.substr(temp.find(' ')+1);
+//            cout << temp.substr(0, temp.find(' '));
+            while(getline(flujo,temp) && tempi < nCasos){
+                for(int i = 0; i< nCasos; i++){
+                    flujos[tempi][tempj] = atoi(temp.substr(0,temp.find(' ')).c_str());
+                    tempj++;
+                    temp = temp.substr(temp.find(' ')+1);                    
+                }
+                tempi++;
+                tempj = 0;
+            }
+            cout << "FLUJOS" << endl;
+            for(int i = 0;i < nCasos; i++){
+                for(int j = 0;j < nCasos; j++){
+                    cout << flujos[i][j] << " ";
+                }
+                cout << endl;
+            }
+            
+            int **distancias;
+            distancias=new int*[nCasos];
+            for(int i = 0; i < nCasos; i++){
+                distancias[i] = new int[nCasos];
+            }
+            
+//            getline(flujo, temp);
+//            cout << temp;
+            
+            tempi = 0;
+            tempj = 0;
+            while(getline(flujo,temp) && tempi < nCasos){
+                for(int i = 0; i< nCasos; i++){
+                    distancias[tempi][tempj] = atoi(temp.substr(0,temp.find(' ')).c_str());
+                    tempj++;
+                    temp = temp.substr(temp.find(' ')+1);                    
+                }
+                tempi++;
+                tempj = 0;
+            }
+            
+            cout << endl << "DISTANCIAS" << endl;
+            for(int i = 0;i < nCasos; i++){
+                for(int j = 0;j < nCasos; j++){
+                    cout << distancias[i][j] << " ";
+                }
+                cout << endl;
+            }
         }
         //     char c = flujo.get();
         //    while (flujo.good()) {
@@ -47,7 +96,7 @@ int main(int argc, char** argv) {
     } else {
         cout << "Error opening file";
     }
-    cout << nCasos;
+    //cout << nCasos;
     
     
     
