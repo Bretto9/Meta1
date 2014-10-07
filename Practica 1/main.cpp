56b1ae611699ed2c42fe38ea992eccd833114bd1
Bretto9 <jose.bretto@gmail.com>
2014-10-07 17:25:43 +0200

    Inicio practica
    Inicio

---
+++ Practica 1/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: JoseDavid
+ *
+ * Created on 7 de octubre de 2014, 17:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
